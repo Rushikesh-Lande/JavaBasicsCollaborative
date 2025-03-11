2515fcfb9cb0177120c0fb0fa04b4d119f07d6b7
VishalDutonde <130062054+VishalDutonde@users.noreply.github.com>
2025-03-11 22:25:48 +0530

    Reverse Array

---
+++ JavaProblemByVishal/ReverseArray.java
@@ -0,0 +1,29 @@
+// reverse the array ?
+import java.util.Arrays;
+
+public class ReverseArray {
+
+   public int[] reverse(int[] arr )
+   {
+	    int[] arr1= {10,20,30,40}; 
+		int [] arr2= new int[arr1.length];
+
+		int a=0; 
+		for(int i=arr1.length-1; i>=0; i--)
+		{
+			arr2[a]=arr1[i];
+			a++;	
+		}    
+		//System.out.println(Arrays.toString(arr2)); 
+		return arr2;
+   }
+
+	public static void main(String[] args) {
+		ReverseArray ra = new ReverseArray();
+        int[] arr = {10, 20, 30, 40}; 
+        int[] reversedArr = ra.reverse(arr); 
+        System.out.println(Arrays.toString(reversedArr));
+
+	}
+
+}
