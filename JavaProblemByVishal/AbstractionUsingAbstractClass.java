8719cbff4cd315f568ab22b09e78dc899514df3d
VishalDutonde <130062054+VishalDutonde@users.noreply.github.com>
2025-03-17 16:47:39 +0530

    abstraction using abstract class

---
+++ JavaProblemByVishal/AbstractionUsingAbstractClass.java
@@ -0,0 +1,36 @@
+ abstract class Vehicle {
+
+	public abstract void start();
+	public void stop() {
+		System.out.println("vehicle is stoped.....");
+	}
+}
+
+class Car extends Vehicle{
+	@Override
+	public  void start()
+	{
+		 System.out.println("My Car is Start with button ....");
+
+	}
+	
+}
+class Bike extends Vehicle{
+	@Override
+	public void start()
+	{
+	      System.out.println("My Bike is Start with Kick ....");
+
+	}
+}
+class AbstractionUsingAbstractClass{
+	public static void main(String[] args)
+	{
+	Car car = new Car();
+	Bike bike = new Bike();
+	car.start();
+	car.stop();
+	bike.start();
+	bike.stop();
+	}
+}
