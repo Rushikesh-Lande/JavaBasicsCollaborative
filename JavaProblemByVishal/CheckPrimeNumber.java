c547037a97babfbda09d4a4245b984de890a9bc3
VishalDutonde <130062054+VishalDutonde@users.noreply.github.com>
2025-06-13 10:15:48 +0530

    check given prime number

--- JavaProblemByVishal/CheckPrimeNumber.java
+++ JavaProblemByVishal/CheckPrimeNumber.java
@@ -1,24 +1,38 @@
 //Write a Java program to check if the given number is a prime number?
-public class CheckPrimeNumber
+public class CheckPrimeNumber 
 {
-  public void isPrimeNumberPresent(int num)
- {
-   for (int i=2; i<=num-1;i++)
-   {
-    if(num%i==0)
-    {
-         System.out.println(+num +" isNonPrime Number"); 
-         break;     
-    }else {
-         System.out.println(+num +" isPrime Number"); 
-         break;         
-    }
-   }     
- }
 
+public void isPrimeNumber(int num)
+{
+
+if(num<=1)
+{
+System.out.println("Non-Prime");
+return;
+}
+
+Boolean isPrime=true;
+for(int i=2; i<num;i++)
+{
+if(num%i==0){
+  isPrime=false;
+  break;
+}
+}
+
+if(isPrime)
+{
+System.out.println("Prime");
+
+}else{
+System.out.println("Non-Prime");
+
+}
+
+}
 public static void main(String[] args)
 {
-   CheckPrimeNumber primeNum = new CheckPrimeNumber();
-   primeNum.isPrimeNumberPresent(6);
+CheckPrimeNumber cpn = new CheckPrimeNumber();
+cpn.isPrimeNumber(15);
 }
 }
